b629d9ced0b802d6d80b4eb9be4378ce6bf629e2
Marijus Kundelis <markakk@gmail.com>
2022-09-07 18:56:21 +0300

    Fix map load

--- hrx-delivery-woo/assets/js/front/init-map.js
+++ hrx-delivery-woo/assets/js/front/init-map.js
@@ -1,42 +1,43 @@
 (function($) {
 
     window.hrxMap = {
+        lib: null,
         init: function( container, terminals ) {
-            const tmjs = new TerminalMapping();
+            this.lib = new HrxMapping();
             
             let method_key = container.dataset.method;
             let selected_field = document.getElementById("hrx-method-" + method_key + "-selected");
 
-            tmjs.setImagesPath(hrxGlobalVars.img_url + "map/");
-            tmjs.setTranslation(hrxGlobalVars.txt.mapModal);
-            tmjs.dom.setContainerParent(container);
+            this.lib.setImagesPath(hrxGlobalVars.img_url + "map/");
+            this.lib.setTranslation(hrxGlobalVars.txt.mapModal);
+            this.lib.dom.setContainerParent(container);
             
-            tmjs.setParseLocationName(( location ) => {
+            this.lib.setParseLocationName(( location ) => {
                 return location.name;
             });
 
-            tmjs.setParseMapTooltip(( location, leafletCoords ) => {
+            this.lib.setParseMapTooltip(( location, leafletCoords ) => {
                 return location.address + ', ' + location.city;
             });
 
-            tmjs.sub("tmjs-ready", function(data) {
+            this.lib.sub("tmjs-ready", function(data) {
                 let selected_location = data.map.getLocationById(selected_field.value);
                 if (typeof(selected_location) != 'undefined' && selected_location != null) {
-                    tmjs.dom.setActiveTerminal(selected_location);
-                    tmjs.publish('terminal-selected', selected_location);
+                    hrxMap.lib.dom.setActiveTerminal(selected_location);
+                    hrxMap.lib.publish('terminal-selected', selected_location);
                 }
             });
 
-            tmjs.sub("terminal-selected", function(data) {
+            this.lib.sub("terminal-selected", function(data) {
                 selected_field.value = data.id;
-                tmjs.dom.setActiveTerminal(data.id);
-                tmjs.publish("close-map-modal");
+                hrxMap.lib.dom.setActiveTerminal(data.id);
+                hrxMap.lib.publish("close-map-modal");
                 console.log("HRX: Saving selected terminal...");
                 hrxAjax.save_terminal(data.id, "add_terminal");
                 console.log("HRX: Terminal changed to " + data.id);
             });
 
-            tmjs.init({
+            this.lib.init({
                 country_code: container.dataset.country,
                 identifier: "hrx",
                 isModal: true,
@@ -44,8 +45,30 @@
                 hideContainer: true,
                 hideSelectBtn: true,
                 cssThemeRule: "tmjs-default-theme",
-                terminalList: terminals
+                terminalList: terminals,
             });
+
+            hrxMap.update_list();
+        },
+
+        update_list: function() {
+            var selected_postcode = this.get_postcode();
+
+            this.lib.dom.searchNearest(selected_postcode);
+            this.lib.dom.UI.modal.querySelector('.tmjs-search-input').value = selected_postcode;
+        },
+
+        get_postcode: function() {
+            var postcode = "";
+            var ship_to_dif_checkbox = document.getElementById("ship-to-different-address-checkbox");
+            
+            if ( typeof(ship_to_dif_checkbox) != "undefined" && ship_to_dif_checkbox != null && ship_to_dif_checkbox.checked ) {
+                postcode = document.getElementById("shipping_postcode").value;
+            } else {
+                postcode = document.getElementById("billing_postcode").value;
+            }
+
+            return postcode;
         }
     };
 
